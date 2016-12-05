90317b22b70e0ee55fcf3f5fb73cc73d87ad0e32
UnnoTed <unnotedx@gmail.com>
2016-12-04 23:24:05 -0300

    Add HP Buff flash

--- app/app/components/flash/flash.directive.js
+++ app/app/components/flash/flash.directive.js
@@ -7,6 +7,7 @@ export function FlashDirective($interval, $timeout) {
     scope: {
       enabled: '=',
       start: '=',
+      mode: '@',
       end: '='
     }
   };
@@ -14,24 +15,31 @@ export function FlashDirective($interval, $timeout) {
   return directive;
 
   function linkFunc(scope, el, attrs) {
+    var start = angular.isDefined(scope.start) ? scope.start.color : '';
+    var end = angular.isDefined(scope.end) ? scope.end.color : '';
+
     var switcher = false;
     var mi;
 
     var run = function() {
       mi = $interval(function() {
         angular.element(el).animate({
-          'background-color': (switcher ? scope.start : scope.end).color,
+          'background-color': switcher ? start : end,
         }, 150);
 
         switcher = !switcher;
       }, 300);
     };
 
-    var stop = function() {
+    var stop = function(cb) {
       $interval.cancel(mi);
 
       $timeout(function() {
         angular.element(el).removeAttr('style');
+
+        if (angular.isFunction(cb)) {
+          cb();
+        }
       }, 301);
     };
 
@@ -42,6 +50,26 @@ export function FlashDirective($interval, $timeout) {
         stop();
       }
     });
+
+    scope.$watch('start', function() {
+      if (angular.isDefined(scope.start)) {
+        start = scope.start.color;
+
+        if (angular.isDefined(scope['mode']) && scope['mode'] === 'alpha') {
+          start = angular.element.Color(scope.start.color).transition('transparent', 0.3).toRgbaString();
+        }
+      }
+    }, true);
+
+    scope.$watch('end', function() {
+      if (angular.isDefined(scope.end)) {
+        end = scope.end.color;
+
+        if (angular.isDefined(scope['mode']) && scope['mode'] === 'alpha') {
+          end = angular.element.Color(scope.end.color).transition('transparent', 0.4).toRgbaString();
+        }
+      }
+    }, true);
 
     scope.$on('$destroy', function() {
       stop();
