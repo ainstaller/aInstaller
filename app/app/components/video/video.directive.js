b4017053814ddb272399d7c4e5be8809eb7f880c
UnnoTed <UnnoTedx@gmail.com>
2019-05-29 13:10:58 -0300

    Fix background video

--- app/app/components/video/video.directive.js
+++ app/app/components/video/video.directive.js
@@ -13,43 +13,43 @@ export function VideoDirective($timeout, $rootScope) {
       return;
     }
 
-    var options = {
-      videoURL: 'https://www.youtube.com/watch?v=3YBsmwZi56s', 
-      showControls: false, 
-      quality: 'highres', 
-      autoPlay: true, 
-      optimizeDisplay: true, 
-      mute: true, 
-      startAt: 0,
-      opacity: 1, 
-      align: 'center, center'
-    };
+    // var options = {
+    //   videoURL: 'https://www.youtube.com/watch?v=3YBsmwZi56s', 
+    //   showControls: false, 
+    //   quality: 'highres', 
+    //   autoPlay: true, 
+    //   optimizeDisplay: true, 
+    //   mute: true, 
+    //   startAt: 0,
+    //   opacity: 1, 
+    //   align: 'center, center'
+    // };
 
-    scope.$watch('settings.video_url', function() {
-      if (angular.isDefined(scope.settings.video_url) && scope.settings.video_url.length > 0) {
-        $timeout(function() {
-          options.videoURL = scope.settings.video_url;
-          if (angular.element('#bgvideo').hasClass('mb_YTPlayer')) {
-            angular.element('#bgvideo').YTPChangeMovie(options);
-          } else {
-            angular.element(el).YTPlayer(options);
-          }
-        }, 1000);
-      }
-    });
+    // scope.$watch('settings.video_url', function() {
+    //   if (angular.isDefined(scope.settings.video_url) && scope.settings.video_url.length > 0) {
+    //     $timeout(function() {
+    //       options.videoURL = scope.settings.video_url;
+    //       if (angular.element('#bgvideo').hasClass('mb_YTPlayer')) {
+    //         angular.element('#bgvideo').YTPChangeMovie(options);
+    //       } else {
+    //         angular.element(el).YTPlayer(options);
+    //       }
+    //     }, 1000);
+    //   }
+    // });
 
-    scope.$watch('settings.video', function() {
-      if ($rootScope.settings.video === true) {
-        $timeout(function() {
-          angular.element(el).YTPPlay();
-          angular.element('.mbYTP_wrapper').show();
-        }, 1000);
-      } else {
-        $timeout(function() {
-          angular.element(el).YTPPause();
-          angular.element('.mbYTP_wrapper').hide();
-        }, 1000);
-      }
-    });
+    // scope.$watch('settings.video', function() {
+    //   if ($rootScope.settings.video === true) {
+    //     $timeout(function() {
+    //       angular.element(el).YTPPlay();
+    //       angular.element('.mbYTP_wrapper').show();
+    //     }, 1000);
+    //   } else {
+    //     $timeout(function() {
+    //       angular.element(el).YTPPause();
+    //       angular.element('.mbYTP_wrapper').hide();
+    //     }, 1000);
+    //   }
+    // });
   }
 }
--- app/app/index.run.js
+++ app/app/index.run.js
@@ -1,6 +1,7 @@
 const storage = require('electron-json-storage');
+const path = require('path');
 
-export function runBlock ($rootScope, $log, $interval) {
+export function runBlock($rootScope, $log, $interval) {
   'ngInject';
   $log.debug('runBlock end');
 
@@ -14,41 +15,41 @@ export function runBlock ($rootScope, $log, $interval) {
     showSelectionPalette: true,
     localStorageKey: 'ainstaller.colors',
     palette: [
-      ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
-      ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"],
-      ["#f4cccc","#fce5cd","#fff2cc","#d9ead3","#d0e0e3","#cfe2f3","#d9d2e9","#ead1dc"],
-      ["#ea9999","#f9cb9c","#ffe599","#b6d7a8","#a2c4c9","#9fc5e8","#b4a7d6","#d5a6bd"],
-      ["#e06666","#f6b26b","#ffd966","#93c47d","#76a5af","#6fa8dc","#8e7cc3","#c27ba0"],
-      ["#c00","#e69138","#f1c232","#6aa84f","#45818e","#3d85c6","#674ea7","#a64d79"],
-      ["#900","#b45f06","#bf9000","#38761d","#134f5c","#0b5394","#351c75","#741b47"],
-      ["#600","#783f04","#7f6000","#274e13","#0c343d","#073763","#20124d","#4c1130"]
+      ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
+      ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"],
+      ["#f4cccc", "#fce5cd", "#fff2cc", "#d9ead3", "#d0e0e3", "#cfe2f3", "#d9d2e9", "#ead1dc"],
+      ["#ea9999", "#f9cb9c", "#ffe599", "#b6d7a8", "#a2c4c9", "#9fc5e8", "#b4a7d6", "#d5a6bd"],
+      ["#e06666", "#f6b26b", "#ffd966", "#93c47d", "#76a5af", "#6fa8dc", "#8e7cc3", "#c27ba0"],
+      ["#c00", "#e69138", "#f1c232", "#6aa84f", "#45818e", "#3d85c6", "#674ea7", "#a64d79"],
+      ["#900", "#b45f06", "#bf9000", "#38761d", "#134f5c", "#0b5394", "#351c75", "#741b47"],
+      ["#600", "#783f04", "#7f6000", "#274e13", "#0c343d", "#073763", "#20124d", "#4c1130"]
     ]
   };
 
   // default settings
   $rootScope.settings = {
     video: true,
-    video_url: 'https://www.youtube.com/watch?v=3YBsmwZi56s'
+    video_url: path.join(__dirname, "background.mp4"),
   };
 
   $rootScope.loaded = false;
-  storage.has('settings', function(error, has) {
+  storage.has('settings', function (error, has) {
     if (error) throw error;
 
     // load settings
-    storage.getMany(['settings', 'crosshairs', 'colors', 'styles'], function(error, data) {
+    storage.getMany(['settings', 'crosshairs', 'colors', 'styles'], function (error, data) {
       if (error) throw error;
 
       $rootScope.crosshairs = data.crosshairs;
 
-      if (has) {
-        $rootScope.settings = data.settings;
-      } else {
-        $rootScope.settings = {
-          video: true,
-          video_url: 'https://www.youtube.com/watch?v=3YBsmwZi56s'
-        };
-      }
+      // if (has) {
+      // $rootScope.settings = data.settings;
+      // } else {
+      // $rootScope.settings = {
+      //   video: true,
+      //   video_url: require("./background.mp4"),
+      // };
+      // }
 
       $rootScope.colors = data.colors;
       $rootScope.styles = data.styles;
@@ -58,7 +59,7 @@ export function runBlock ($rootScope, $log, $interval) {
 
 
 
-  $rootScope.sendEvent = function(name) {
+  $rootScope.sendEvent = function (name) {
     if (name === 'delete' || name === 'reset') {
       if (confirm('Do you really wanna ' + name + ' it?') === false) {
         return;
@@ -69,7 +70,7 @@ export function runBlock ($rootScope, $log, $interval) {
     $rootScope.$broadcast(name);
   };
 
-  $rootScope.$watch('crosshairs', function() {
+  $rootScope.$watch('crosshairs', function () {
     if (angular.isObject($rootScope.crosshairs)) {
       $rootScope.totalCrosshairs = Object.keys($rootScope.crosshairs).length;
     } else {
@@ -77,12 +78,12 @@ export function runBlock ($rootScope, $log, $interval) {
     }
   }, true);
 
-  
-  var randomNumber = function(min, max) {
+
+  var randomNumber = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
 
-  $interval(function() {
+  $interval(function () {
     if (angular.isUndefined($rootScope.color)) {
       return;
     }
